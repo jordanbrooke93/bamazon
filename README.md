5713979605472dc9c5ded263cf996e89fbec5ff1
jordanbrooke93 <jordantaylor93@me.com>
2018-10-14 11:30:51 -0400

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # bamazon
+
+## What is Bamazon?
+Bamazon is an online shopping experience that the user a list of the products offered. 
+
+
+## How to use it
+Once the user has read the list, they are asked to input the item id of the item that they would like to purchase. 
+
+![bamazon](images/itemsdisplay.png)
+
+Once they have answered that question, they are then asked how many of that item would they like to buy.
+
+![bamazon](images/quantityinput.png)
+
+ The database is then checked to see if bamazon has that quantity in stock. If so, the customer is thanked for their purchase and told their total.
+
+ ![bamazon](images/purchase.png)
+ 
+If that quantity is not in stock, the user is informed that we do no have it in stock.
+
+![bamazon](images/cantbuy.png)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,34 +16,44 @@ connection.connect(function (err) {
 });
 
 function promptPurchase() {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "item_id",
-            message: "Please enter the item id that you would like to purchase."
-        },
-        {
-            type: "input",
-            name: "quantity",
-            message: "How many would you like to buy?"
+    var query = connection.query("Select * FROM products", function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
         }
-    ]).then(function (answer) {
-        connection.query("SELECT * FROM products", function (err, res) {
-            // for (var i = 0; i < res.length; i++) {
-            // console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
-            var item = answer.item_id
-            var quantity = answer.quantity
-            var price = quantity * res[0].price// 
-            if (quantity <= res[0].stock_quantity) {
-                var quantityUpdate = "UPDATE products SET stock_quantity = " + res[0].stock_quantity - quantity + " WHERE item_id = " + item
-                connection.query(quantityUpdate, function(err, res){
-                    console.log("Thank you for your order! Youre total is $" + price + "!")
-                })
-            } else {
-                console.log("I'm sorry, that item is not in stock")
-            }
-            connection.end()
-            // }
-        })
+        promptPurchase()
     })
+
+    function promptPurchase() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "item_id",
+                message: "Please enter the item id that you would like to purchase."
+            },
+            {
+                type: "input",
+                name: "quantity",
+                message: "How many would you like to buy?"
+            }
+        ]).then(function (answer) {
+            connection.query("SELECT * FROM products", function (err, res) {
+                // for (var i = 0; i < res.length; i++) {
+                // console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+                var item = answer.item_id
+                var quantity = answer.quantity
+                var price = quantity * res[0].price// 
+                if (quantity <= res[0].stock_quantity) {
+                    var quantityUpdate = "UPDATE products SET stock_quantity = " + res[0].stock_quantity - quantity + " WHERE item_id = " + item
+                    connection.query(quantityUpdate, function (err, res) {
+                        console.log("Thank you for your order! Youre total is $" + price + "!")
+                    })
+                } else {
+                    console.log("I'm sorry, that item is not in stock")
+                }
+                connection.end()
+                // }
+            })
+        })
+    }
 }
